9f680582bfe75aeaa636801b1c54325c6e264180
이민수 <minsoo.james1101@gmail.com>
2025-05-08 00:48:17 +0900

    thread on flask

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from automation.automator import start_task
 from web.server import run_flask
 import time
+import threading
 
 if __name__ == '__main__':
     print("프로그램을 실행합니다")
     print("작업을 수행합니다")
-    run_flask()
+    # run_flask()
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.daemon = True  # 프로그램 종료 시 서버도 종료되도록 설정
+    flask_thread.start()
     start_task()
 
     try:
