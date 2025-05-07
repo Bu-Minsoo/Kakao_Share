5b5dcbad5807fff486ecab20df0f8a41d71e6a17
이민수 <minsoo.james1101@gmail.com>
2025-05-07 18:45:17 +0900

    task <-> server

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ def share():
     if flask.request.method == "HEAD":
         return "", 200  # 헬스 체크용 빈 응답
 
+    print("랜더링을 시작합니다.")
+
     title = cr.news_list['title'].strip().replace('"', '').replace("'", "")
     body = cr.news_list['body'].strip().replace('"', '').replace("'", "")
 
     link = cr.news_list['link']
-    print("link = " + link)
 
     return render_template('text.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa',
                            title=title, body=body, link=link)
@@ -24,10 +25,10 @@ def share():
 
 if __name__ == '__main__':
     print("프로그램을 실행합니다")
+    start_task()
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
     print("작업을 수행합니다")
-    start_task()
 
     try:
         while True:
